e2e5e4eb62d964ac2e8bbcc2bc793fb21e5b2ca3
Andy Gu <gua6@uci.edu>
2025-12-06 14:13:12 -0800

    Added normalization functionality

--- src/normalization/dates.py
+++ src/normalization/dates.py
@@ -1,5 +1,25 @@
 from datetime import date
 from typing import Optional
+import re
+from dateutil import parser
 
 def parse_date(date_str: str) -> Optional[date]:
+    if not date_str:
+        return None
+    
+    # Straightforward Parsing
+    try:
+        dt = parser.parse(date_str, fuzzy=True, dayfirst=False)
+        return dt.date()
+    except (ValueError, TypeError, OverflowError):
+        pass
+
+    # Handle any letters that parser doesn't like
+    try:
+        cleaned_str = re.sub(r'(\d+)(st|nd|rd|th)', r'\1', date_str, flags=re.IGNORECASE)
+        dt = dt = parser.parse(cleaned_str, fuzzy=True, dayfirst=False)
+        return dt.date()
+    except (ValueError, TypeError, OverflowError):
+        pass
+
     return None
