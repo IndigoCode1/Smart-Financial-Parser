347ecc5a1df4bea42ce44af1e91f55546e81e459
Andy Gu <gua6@uci.edu>
2025-12-06 14:13:15 -0800

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -11,11 +11,17 @@ def test_us_short_format():
 def test_human_readable():
     assert parse_date("Feb 26, 2025") == date(2025, 2, 26)
 
+def reversed_test_human_readable():
+    assert parse_date("2025 27 feb") == date(2025, 2, 27)
+
 def test_dot_format():
     assert parse_date("2025.10.22") == date(2025, 10, 22)
 
-def test_difficult_human_format():
+def test_human_format():
     assert parse_date("Sept. 3rd, 2024") == date(2024, 9, 3)
+
+def test_difficult_human_format():
+    assert parse_date("Feb ury 19th, 2024") == date(2024, 2, 19)
 
 def test_invalid_date_returns_none():
     assert parse_date("2025/13/01") is None
