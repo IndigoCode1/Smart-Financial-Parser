8b9c9fa256893008571b712332ce33e0fa1b95a4
Andy Gu <gua6@uci.edu>
2025-12-06 13:27:42 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import argparse
+from src.pipeline import FinancialPipeline
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input", type=str, default="data/raw/generated_transactions.csv", help="Path to input CSV")
+    parser.add_argument("--output", type=str, default="data/processed/clean_data.csv", help="Path to output CSV")
+
+    args = parser.parse_args()
+
+    pipeline = FinancialPipeline(input_path=args.input, output_path=args.output)
+    pipeline.run()
+
+if __name__ == "__main__":
+    main()
